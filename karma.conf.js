6f085b378dda2329f66e51ead978cd3ec544815f
Cristian Talau <cristitalau@gmail.com>
2015-07-04 23:33:19 +0300

    Changed the tests reporter to dots

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
       { pattern: 'node_modules/react/dist/**/*', included: false}
     ],
 
-    reporters: ['progress'],
+    reporters: ['dots'],
 
     port: 9876,
 

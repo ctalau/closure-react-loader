87a2cd8d2e532102147ea8581df78b6f02c11936
Cristian Talau <cristi_talau@oxygenxml.com>
2015-07-07 16:06:09 +0300

    Added source maps support

--- closure-react-loader.js
+++ closure-react-loader.js
@@ -5,18 +5,33 @@ CLOSURE_IMPORT_SCRIPT = function(src) {
     xmlhttp.onreadystatechange = function() {
       if (xmlhttp.readyState == XMLHttpRequest.DONE) {
         if (xmlhttp.status == 200) {
-          var script = JSXTransformer.transform(xmlhttp.responseText).code;
-          document.write('<script>' + script + '</script>');
+          var content = xmlhttp.responseText;
+          var transformResult = JSXTransformer.transform(content, {sourceMap: true});
+          var script = transformResult.code;
+          var sourceMap = transformResult.sourceMap;
+          if (typeof window.btoa !== "undefined") {
+            sourceMap.files = [null];
+            sourceMap.file = src;
+            sourceMap.sourcesContent = [content];
+            var sourceMapUrl = "//# sourceMappingURL=data:application/json;base64," + btoa(JSON.stringify(sourceMap));
+            script += sourceMapUrl;
+            var scriptUrl = "data:text/javascript;base64," + btoa(script);
+            document.write('<script src="' + scriptUrl + '"></script>');
+          } else {
+            document.write('<script>' + script + '</script>')
+          }
           written = true;
         }
-      } 
+      } else {
+        console.log('Error loading script "' + src + '": ' + xmlhttp.status);
+      }
     }
     xmlhttp.open("GET", src, false);
     xmlhttp.send();
   } else {
-    document.write(
-          '<script type="text/javascript" src="' + src + '"></' + 'script>');
+     document.write(
+       '<script type="text/javascript" src="' + src + '"></' + 'script>');
     written = true;
   }
   return written;
-}
+};

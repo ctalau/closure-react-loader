b6d3b7a478f85e40cb202376900a403ea9d2f6bf
Cristian Talau <cristitalau@gmail.com>
2015-07-07 21:49:04 +0300

    Added several saucelabs platforms for tests

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
       { pattern: 'node_modules/react/dist/**/*', included: false}
     ],
 
-    reporters: ['dots'],
+    reporters: ['dots', 'saucelabs'],
 
     port: 9876,
 
@@ -23,8 +23,32 @@ module.exports = function(config) {
     autoWatch: false,
 
     // start these browsers
-    browsers: ['Firefox'],
-
+    browsers: ['Firefox', 'sl_chrome', 'sl_ios_safari', 'sl_ie_11'],
+    customLaunchers: {
+      sl_chrome: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 7',
+        version: '35'
+      },
+      sl_ios_safari: {
+        base: 'SauceLabs',
+        browserName: 'iphone',
+        platform: 'OS X 10.9',
+        version: '7.1'
+      },
+      sl_ie_11: {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 8.1',
+        version: '11'
+      }
+    },
+    captureTimeout: 120000,
+    sauceLabs: {
+        testName: 'closure-react-loader Unit Tests'
+    },
+    
     // Continuous Integration mode
     singleRun: false 
   })
